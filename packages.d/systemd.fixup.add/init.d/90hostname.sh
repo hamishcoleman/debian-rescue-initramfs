7884e2b28ee6c46480d493531014628bc13f3d38
Hamish Coleman <hamish@zot.org>
2017-07-04 18:33:12 +0800

    Allow different hostname prefixes and try using a serial number for static data

--- packages.d/systemd.fixup.add/init.d/90hostname.sh
+++ packages.d/systemd.fixup.add/init.d/90hostname.sh
@@ -1,8 +1,14 @@
 # A default hostname
 #
+# Copyright (C) 2017 Hamish Coleman <hamish@zot.org>
 
 # Only do this if there is not already a hostname configured
 if [ ! -f /etc/hostname ]; then
+
+    # Allow earlier config files to specify a different hostname prefix
+    if [ -z "$HOST_PREFIX" ]; then
+        HOST_PREFIX=ramdisk
+    fi
 
     # First, Try to create a stable hostname from our ethernet addr
     hostid=`ip link show dev eth0 |grep ether`
@@ -13,13 +19,18 @@ if [ ! -f /etc/hostname ]; then
     fi
 
     if [ -z "$hostid" ]; then
+        # No network cards, try looking for a serial number
+        hostid=`grep ^Serial /proc/cpuinfo`
+    fi
+
+    if [ -z "$hostid" ]; then
         # Nothing worked, use some (bogus this soon after boot) randomness
         hostid=`dd if=dev/urandom bs=16 count=1`
     fi
 
     hash=`echo $hostid | sha1sum | cut -c1-8`
 
-    echo "ramdisk-$hash" >/etc/hostname
+    echo "$HOST_PREFIX-$hash" >/etc/hostname
 fi
 
 
